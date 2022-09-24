77f930d8961179e6e820e28d6af7faca8c721da2
nuurlzh <112592377+nuurlzh@users.noreply.github.com>
2022-09-25 06:24:43 +0700

    Add files via upload

---
+++ 2217051022_Nurul Izzah_Praktest2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	cout <<"Program menampilkan kata dari angka yang diinput\n";
+	
+	int angka;
+	cout << "Input Angka: ";
+	cin >> angka;
+	
+	if(angka==1){
+		cout <<"Satu";
+	}else if(angka==2){
+		cout <<"Dua";
+	}else if(angka==3){
+		cout <<"Tiga";
+	}else if(angka>3){
+		cout <<"Bilangan lebih dari tiga";
+	}
+		
+
+}
+	
